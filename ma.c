649df803ea94cbc20b2a80f3441a660f3dfdfac1
João Nuno Abreu <joao.nuno.escola@gmail.com>
2019-04-14 18:59:36 +0100

    Melhoramento do i do ma

--- ma.c
+++ ma.c
@@ -18,21 +18,33 @@ ssize_t readln(int fildes, void *buf, size_t nbyte){
     return i;
 }
 
+#define BUFFSIZE 10000
+
 int main(){
 
-    int BUFFSIZE = 10000;
-    int fd = open("ARTIGOS.txt", O_CREAT | O_WRONLY | O_APPEND, 0666);
+    int fd = open("ARTIGOS.txt", O_CREAT | O_TRUNC | O_WRONLY, 0666); // Cria um novo sempre
     char *buf = malloc(BUFFSIZE);
     int line = 1;
-    if(fd == -1){write(1,"ERRO\n",5); exit(1);}
+    if(fd == -1) {write(1,"ERRO\n",5); exit(1);}
 
     while(1){
         int pre = sprintf(buf,"%d ",line);
         ssize_t n = readln(0,buf+pre,BUFFSIZE-pre); // Código numérico + linha lida
         if (n <= 0) break;
-        if(buf[pre] == 'i'){
-            write(fd,buf,n+pre);
-            line++;
+        switch(buf[pre]){
+            case 'i': 
+                write(fd,buf,n+pre); // Escreve no ficheiro artigos.txt a linha + número
+                char charLinha[BUFFSIZE];
+                sprintf(charLinha,"O número é: %d\n",line);
+                write(1,charLinha,strlen(charLinha));
+                line++;
+                break;
+            case 'o': 
+                break;
+            case 'p':
+                break;
+            default:
+                write(1,"Formato errado\n",16);
         }
     }
     return 0;
